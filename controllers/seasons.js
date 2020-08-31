128e58b4918a99f896c57332640589b2a6739f5e
Cory Spicer <corys4139@gmail.com>
2020-08-31 15:55:50 -0400

    add bootstrap

--- controllers/seasons.js
+++ controllers/seasons.js
@@ -1,13 +1,26 @@
 const Season = require('../models/season')
+const { default: Axios } = require('axios')
 
 module.exports = {
-    index
+    index,
+    show
 }
 
 function index(req, res) {
     Season.find({}).then((seasons) => {
-        res.render('seasons/index', { title: 'Season Index',
-        user: req.user, 
-        seasons})
+        res.render('seasons/index', { 
+            title: 'Season Index',
+            user: req.user, 
+            seasons})
+    })
+}
+
+function show(req, res) {
+    Season.findById(req.params.id, (err, season) => {
+        res.render('seasons/show', {
+            title: 'Season Details', 
+            user: req.user, 
+            season
+        })
     })
 }
--- routes/seasons.js
+++ routes/seasons.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const seasonsCtrl = require('../controllers/seasons');
 
 router.get('/', isLoggedIn, seasonsCtrl.index)
+router.get('/:id', isLoggedIn, seasonsCtrl.show);
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
