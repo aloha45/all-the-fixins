f9bd0ce0bd67f85800bbaaeb9dfec90c2ad3ba9c
Cory Spicer <corys4139@gmail.com>
2020-09-02 11:52:57 -0400

    add message index

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,9 +1,21 @@
 const Message = require('../models/message')
 
 module.exports = {
-    index
+    index,
+    create
 }
 
 function index (req, res) {
-    
+    Message.find({})
+    .then ((messages) => {
+        res.render("messages/index", {
+            title: "Message Board",
+            user: req.user, 
+            messages: messages.reverse()
+        })
+    })
+}
+
+function create (req, res) {
+
 }
--- routes/messages.js
+++ routes/messages.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
-const messagesCtrl = require('../controllers/messages')
+const messagesCtrl = require('../controllers/messages');
+
+router.get('/', messagesCtrl.index);
+router.post('/', messagesCtrl.create);
 
 module.exports = router;
