e7ed3d22884546f1a99ee6c99fa7a173fd6d17dc
Cory Spicer <corys4139@gmail.com>
2020-09-02 14:25:57 -0400

    replies work

--- controllers/messages.js
+++ controllers/messages.js
@@ -3,7 +3,8 @@ const Message = require('../models/message')
 module.exports = {
     index,
     create,
-    show
+    show,
+    reply
 }
 
 function index (req, res) {
@@ -33,6 +34,18 @@ function show (req, res) {
             title: 'Message Details',
             user: req.user,
             message
+        });
+    });
+}
+
+function reply (req, res) {
+    Message.findById(req.params.id)
+    .then((message) => {
+        req.body.postedBy = req.user.name;
+        req.body.avatar = req.user.avatar;
+        message.replies.push(req.body);
+        message.save().then(() => {
+            res.redirect(`/messages/${req.params.id}`)
         });
     });
 }
