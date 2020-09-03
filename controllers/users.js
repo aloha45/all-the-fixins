ce5d1081136ef88c18cf86e5720222161e2e45d6
Cory Spicer <corys4139@gmail.com>
2020-09-03 15:14:40 -0400

    more styling

--- controllers/users.js
+++ controllers/users.js
@@ -11,7 +11,7 @@ module.exports = {
 
 function index(req, res) {
   User.find({}).then(users => {
-    res.render('users/index', { title: 'User Index', user: req.user, users })
+    res.render('users/index', { title: 'Castaways', user: req.user, users })
   })
 }
 
@@ -20,7 +20,7 @@ function showProfile (req, res) {
   User.findById(req.user._id).populate('allianceMembers')
   .then((user) => {
     res.render('users/profile', { 
-      title: 'User Profile', 
+      title: 'Castaway Profile', 
       user});
   })
 }
